dcd04f99f840f887cb511e9a6079784ae2d936c5
Hugo Álvarez <92876539+hasdev02@users.noreply.github.com>
2023-11-03 16:42:50 +0100

    Delete prisma/migrations directory

--- prisma/migrations/20221109155825_new_sub_types/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The values [CUSTOM] on the enum `User_subscription` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `user` MODIFY `subscription` ENUM('INACTIVE', 'STANDARD', 'ENTRENAMIENTO', 'DIETA', 'FULL') NOT NULL DEFAULT 'INACTIVE',
-    MODIFY `subscriptionEnd` DATETIME(3) NOT NULL DEFAULT (NOW() + INTERVAL 1 YEAR);
